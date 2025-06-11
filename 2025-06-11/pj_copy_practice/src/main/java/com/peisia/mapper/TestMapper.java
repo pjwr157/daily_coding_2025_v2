2dc3402980111a53fd22e3349765da0692678ee4
pjwr157 <pjwr157@gmail.com>
2025-06-11 17:46:48 +0900

    2025-06-11 학습 내용 추가 (JSON활용 및 pj 복제연습)

---
+++ 2025-06-11/pj_copy_practice/src/main/java/com/peisia/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package com.peisia.mapper;
+
+import com.peisia.spring.dto.TestDto;
+
+public interface TestMapper {
+	public TestDto getData1();
+	public TestDto getData2();
+	public TestDto getData3();
+	public TestDto getData4();
+	
+	public void updateVisitantCount();
+}
